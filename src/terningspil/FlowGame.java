98768543f87446430c0f3b1260571549d90816ee
sniller27 <sniller27@hotmail.com>
2016-09-25 23:05:30 +0200

    new commit

---
+++ src/terningspil/FlowGame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package terningspil;
+
+/**
+ *
+ * @author Soren
+ */
+public class FlowGame {
+
+    public static void main(String[] args) {
+        //Der bliver lavet et objekt/instans af klassen GameEngine, så programmet bliver kørt i main.
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.run();
+    }
+}
